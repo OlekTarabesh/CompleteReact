97dd63cae6fa3a382d733dfb7b398adedba3a504
Olek <oleksandr.tarabesh@gmail.com>
2022-11-20 00:00:34 +0200

    added Fragment and React Router Link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import Home from './components/routes/home/home.component.jsx';
 import {Routes, Route} from 'react-router-dom';
+import Navigation from './components/routes/navigation/navigation.component.jsx';
+const Shop = () => {
+  return (
+    <h1>I am a shop page</h1>
+  );
+}
 
 const App = () => {
 
   return (
     <Routes>
-      <Route path='/home' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
-}
+};
 
 export default App;
