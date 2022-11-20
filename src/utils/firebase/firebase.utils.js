bae0ebfe3c8c533b5b8f1b3c1451c43dddb14654
Olek <oleksandr.tarabesh@gmail.com>
2022-11-20 19:47:34 +0200

    added our sign as well as firebase utils and broght in firebase libraries

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -37,11 +37,7 @@ const firebaseConfig = {
   export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid)
 
-    console.log(userDocRef);
-
     const userSnapshot = await getDoc(userDocRef);
-    console.log(userSnapshot);
-    console.log(userSnapshot.exists());
 
     if(!userSnapshot.exists()) {
         const {displayName, email} = userAuth;
