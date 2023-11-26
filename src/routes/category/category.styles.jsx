8ff605a9a67a6103b9cd84eff35c564c655391e3
OlekTarabesh <oleksandr.tarabesh@gmail.com>
2023-11-26 19:47:26 +0200

    Removed node_modules folder

--- src/routes/category/category.styles.jsx
+++ src/routes/category/category.styles.jsx
@@ -1,10 +1,20 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const CategoryContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   column-gap: 20px;
   row-gap: 50px;
+  @media (max-width: 992px) {
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+  }
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 600px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
 
 export const Title = styled.h2`
@@ -12,4 +22,3 @@ export const Title = styled.h2`
   margin-bottom: 25px;
   text-align: center;
 `;
-
